48cd6db4a0aed4c769bd5300ac459246d248b554
Stanpac <fritschstanislas@gmail.com>
2022-03-16 11:56:59 +0100

    editor qui marche

--- ProtoGrid/Assets/Script/Major Grid and Game Systems/GridGenerator.cs
+++ ProtoGrid/Assets/Script/Major Grid and Game Systems/GridGenerator.cs
@@ -50,64 +50,17 @@ public class GridGenerator : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-#if UNITY_EDITOR
+
 
         if (instantiateGrid)
         {
-
 
             if (grid != null)
             {
                 foreach (GridTiles obj in grid)
                 {
 
-                    DestroyImmediate(obj.gameObject);
-                }
-
-            }
-
-            GridTiles[] list = FindObjectsOfType<GridTiles>();
-            grid = new GridTiles[raws, columns];
-            for (int i = 0; i < list.Length; i++)
-            {
-                int x = (int)list[i].transform.position.x / (int)list[i].transform.localScale.x;
-                int y = (int)list[i].transform.position.z / (int)list[i].transform.localScale.y;
-                grid[x, y] = list[i];
-                grid[x, y].name = "tiles " + x + " " + y;
-            }
-
-            for (int x = 0; x < raws; x++)
-            {
-                for (int y = 0; y < columns; y++)
-                {
-                    if (!grid[x, y])
-                    {
-                        Selection.activeObject = PrefabUtility.InstantiatePrefab(Tile);
-                        var inst = Selection.activeObject as GameObject;
-                        inst.transform.position = new Vector3(x, 0, y);
-                        inst.transform.Find("Renderer").Rotate(90 * Random.Range(0, 5), 90 * Random.Range(0, 5), 90 * Random.Range(0, 5));
-                        grid[x, y] = inst.GetComponent<GridTiles>();
-                        grid[x, y].transform.parent = GameObject.FindGameObjectWithTag("Terrain").transform;
-                        grid[x, y].name = "tiles " + x + " " + y;
-                    }
-                }
-            }
-
-            instantiateGrid = false;
-
-        }
-#endif
-#if !UNITY_EDITOR
-        if (instantiateGrid)
-        {
-
-
-            if (grid != null)
-            {
-                foreach (GridTiles obj in grid)
-                {
-
-                    DestroyImmediate(obj.gameObject);
+                    //DestroyImmediate(obj.gameObject);
                 }
 
             }
@@ -141,10 +94,10 @@ public class GridGenerator : MonoBehaviour
             instantiateGrid = false;
 
         }
-#endif
+
     }
 
-
+    
 
 }
 
