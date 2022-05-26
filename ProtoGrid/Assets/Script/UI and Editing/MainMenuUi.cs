b7bb256ef571eeae5143d2015f438846e303e79a
Stanpac <fritschstanislas@gmail.com>
2022-05-26 18:24:07 +0200

    timeline debut

---
+++ ProtoGrid/Assets/Script/UI and Editing/MainMenuUi.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenuUi : MonoBehaviour
+{
+    [SerializeField] RawImage middleA;
+    [SerializeField] RawImage leftA;
+    [SerializeField] RawImage RightA;
+    [SerializeField] RawImage K;
+    [SerializeField] RawImage R;
+
+    [SerializeField] Vector3 LeftAPos;
+    [SerializeField] Vector3 RightAPos;
+
+    private void Start()
+    {
+        
+    }
+     
+}
+
+
