59a5e9f18ae21df209c81057976cb76c31675da6
Stanpac <fritschstanislas@gmail.com>
2022-05-29 16:36:30 +0200

    transi fin de niveau en cours

--- ProtoGrid/Assets/Script/Major Grid and Game Systems/WorldManager.cs
+++ ProtoGrid/Assets/Script/Major Grid and Game Systems/WorldManager.cs
@@ -12,9 +12,13 @@ public class WorldManager : MonoBehaviour
     [SerializeField] float[] LevelsOfTheWorld;
     GameObject worldLevelUi;
     List<Scene> previewScene;
+    GridTiles[,] grid;
+    DoCoroutine doC;
 
     void Start()
     {
+        grid = FindObjectOfType<GridGenerator>().grid;
+        doC = FindObjectOfType<DoCoroutine>();
         parent = transform.parent.gameObject;
         text = this.transform.Find("Canvas/Name").gameObject.GetComponent<TextMeshProUGUI>();
         gridTiles = parent.GetComponent<GridTiles>();
@@ -33,6 +37,15 @@ public class WorldManager : MonoBehaviour
 
     public void OpenWorld(GameObject button)
     {
-        SceneManager.LoadScene("Lvl_" + Mathf.Floor(LevelsOfTheWorld[int.Parse(button.name)- 1]) + "," + (Mathf.RoundToInt((LevelsOfTheWorld[int.Parse(button.name)- 1] - Mathf.Floor(LevelsOfTheWorld[int.Parse(button.name)- 1])) * 10)), LoadSceneMode.Single);
+        foreach (GridTiles tile in grid)
+        {
+            if (tile != grid[(int)gridTiles.transform.position.x, (int)gridTiles.transform.position.y])
+            {
+                tile.levelTransiIndex = 100;
+
+                doC.startClose(tile, tile.tiling, LevelsOfTheWorld[int.Parse(button.name) - 1], grid[(int)gridTiles.transform.position.x, (int)gridTiles.transform.position.y].GetComponent<GridTiling>());
+            }
+        }
+        //SceneManager.LoadScene("Lvl_" + Mathf.Floor(LevelsOfTheWorld[int.Parse(button.name)- 1]) + "," + (Mathf.RoundToInt((LevelsOfTheWorld[int.Parse(button.name)- 1] - Mathf.Floor(LevelsOfTheWorld[int.Parse(button.name)- 1])) * 10)), LoadSceneMode.Single);
     }
 }
